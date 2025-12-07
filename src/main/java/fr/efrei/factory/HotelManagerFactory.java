e349569d46fd076ae9e7b30bdfa3d3b36159aef3
Laïli <laili.loutchmia@efrei.net>
2025-12-07 18:53:32 +0200

    Update of HotelManagerFactory

--- src/main/java/fr/efrei/factory/HotelManagerFactory.java
+++ src/main/java/fr/efrei/factory/HotelManagerFactory.java
@@ -3,9 +3,9 @@ package fr.efrei.factory;
 import fr.efrei.domain.HotelManager;
 
 public class HotelManagerFactory {
-    public static HotelManager createHotelManager(String id, String name, String username, String email){
-        if (id == null || id.length() < 10 ||
-                name == null || name.isEmpty()){
+    public static HotelManager createHotelManager(String id, String name){
+        if (id == null || id.length() <10 || !id.matches("[A-Z0-9]+") ||
+        name == null || name.isEmpty() || !name.matches("[a-zA-Z]+")){
             return null;
         }
 
